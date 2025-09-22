f7bc4e502ffb991064e3aa4c6eee52f7cb2563cd
Ilgiz <vurganowilgiz@gmail.com>
2025-09-23 01:49:26 +0400

    fix base

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, HashRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App.tsx";
 
 const queryClient = new QueryClient();
--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	base: "/fwt-test-3",
+	base: "/fwt-test-3/",
 });
