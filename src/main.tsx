ee26271d6ed8ccf985004585e2fc96fca7f148ac
Ilgiz <vurganowilgiz@gmail.com>
2025-09-23 01:40:19 +0400

    add HashRouter

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 import App from "./App.tsx";
 
 const queryClient = new QueryClient();
@@ -10,10 +10,10 @@ const queryClient = new QueryClient();
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
 		<QueryClientProvider client={queryClient}>
-			<BrowserRouter>
+			<HashRouter>
 				<App />
 				<ReactQueryDevtools />
-			</BrowserRouter>
+			</HashRouter>
 		</QueryClientProvider>
 	</StrictMode>,
 );
